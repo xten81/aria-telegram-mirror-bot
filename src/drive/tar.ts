0086a48cb5f1345a4625786435cf7da9ee370b60
Ghost <25511024+ihavenoid@users.noreply.github.com>
2020-03-31 19:50:09 +0530

    Update tar.ts

--- src/drive/tar.ts
+++ src/drive/tar.ts
@@ -7,8 +7,7 @@ export function archive(srcPath: string, destName: string, callback: (err: strin
   var targetDirName = `${srcPath.substring(srcPath.lastIndexOf('/') + 1)}`;
   var size = 0;
   writeStream.on('close', () => callback(null, size));
-  writeStream.on('error', (err) => callback(err, size));
-
+  writeStream.on('error', (err: Error) => callback(err.message, size));
   var stream = tar.c(
     {
       // @ts-ignore Unknown property error
@@ -26,4 +25,3 @@ export function archive(srcPath: string, destName: string, callback: (err: strin
 
   stream.pipe(writeStream);
 }
-
